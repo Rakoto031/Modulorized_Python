b6766122a28b82715d7167551a8065ed5a0a09df
Sourcery AI <>
2022-10-28 05:25:05 +0000

    'Refactored by Sourcery'

--- Gaussiandistribution.py
+++ Gaussiandistribution.py
@@ -13,17 +13,12 @@ class Gaussian (Distribution):
         self.mean = avg
         return self.mean
     
-    def calculate_stdev (self, sample=True):
-        if sample: 
-            n =len (self.data) -1
-        else:
-            n = len (self.data)
+    def calculate_stdev(self, sample=True):
+        n = len (self.data) -1 if sample else len (self.data)
         mean = self.calculate_mean()
-        sigma = 0
-        for d in self.data:
-            sigma += (d-mean) **2
+        sigma = sum((d-mean) **2 for d in self.data)
         sigma = math.sqrt(sigma/n)
-        
+
         self.stdev =  sigma
         return self.stdev
     
@@ -37,34 +32,34 @@ class Gaussian (Distribution):
         return (1.0 / (self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
     
     def plot_histogram_pdf(self, n_spaces = 50):
-        
+
         mu = self.mean
         sigma = self.stdev
 
         min_range = min(self.data)
         max_range = max(self.data)
-      
+
         interval = 1.0 * (max_range - min_range) / n_spaces
-        
+
         x =[]
         y =[]
-        
+
         for i in range(n_spaces):
             tmp = min_range + interval*i
             x.append(tmp)
             y.append(self.pdf(tmp))
-        
+
         fig, axes = plt.subplots(2,sharex=True)
         fig.subplots_adjust(hspace=.5)
         axes[0].hist(self.data, density=True)
         axes[0].set_title('Normed Histogram of Data')
         axes[0].set_ylabel('Density')
-        
+
         axes[1].plot(x, y)
         axes[1].set_title('Normal Distribution for \\n Sample Mean and Sample Standard Deviation')
         axes[0].set_ylabel('Density')
         plt.show()
-        
+
         return x, y
     
     def __add__(self, other):
@@ -74,5 +69,5 @@ class Gaussian (Distribution):
         return result
     
     def __repr__(self):
-        return "mean {}, standard deviation {}".format(self.mean, self.stdev)
+        return f"mean {self.mean}, standard deviation {self.stdev}"
     
